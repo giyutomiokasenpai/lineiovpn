2606b301f2912d0d4d25aa5d2fecba877177601f
giyu tomioka <giyu@MacBook-Air-giyu.local>
2025-05-19 23:31:23 +0500

    first

--- fsm_machine/admin/add_server.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class ServerData(StatesGroup):
-    api_url = State()
-    ip = State()
-    password = State()
-    location_country= State()
-    location_city = State()
